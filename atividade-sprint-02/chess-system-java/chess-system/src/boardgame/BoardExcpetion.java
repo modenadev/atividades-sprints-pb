4cf4a471ff47bbd07ccbcc646c6bb1307090a857
Ruan <ruanm0dena@outlook.com>
2024-08-16 18:12:12 -0300

    ChessException and ChessPosition

--- atividade-sprint-02/chess-system-java/chess-system/src/boardgame/BoardExcpetion.java
+++ atividade-sprint-02/chess-system-java/chess-system/src/boardgame/BoardExcpetion.java
@@ -1,6 +1,7 @@
 package boardgame;
 
 public class BoardExcpetion extends RuntimeException {
+    private static final long serialVersionUID = 1L;
     public BoardExcpetion(String msg) {
         super(msg);
     }
