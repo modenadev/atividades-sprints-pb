c5b40ad60e81a075676ba4447e266ed362288de4
Ruan <ruanm0dena@outlook.com>
2024-08-17 16:06:56 -0300

    handling captured pieces

--- atividade-sprint-02/chess-system-java/chess-system/src/application/Program.java
+++ atividade-sprint-02/chess-system-java/chess-system/src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
-
 import boardgame.Board;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -16,12 +17,14 @@ public class Program {
 
         ChessMatch chessMatch = new ChessMatch();
         Scanner sc= new Scanner(System.in);
+        List<ChessPiece> captured = new ArrayList<>();
+
 
         while (true){
 
             try {
                 UI.clearScreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -35,6 +38,11 @@ public class Program {
                 ChessPosition destination = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, destination);
+
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
+
             }catch (ChessException | InputMismatchException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
